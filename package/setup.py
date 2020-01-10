285ad32cd520f1f4a3e1eea9145e750baf7676f1
David Bootle <d.bootle18@psaschool.org>
2020-01-10 13:03:23 -0500

    Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wordsearchutils-TheWeirdSquid", # Replace with your own username
+    name="wordsearchutils",
     version="1.1",
     author="David Bootle",
     author_email="davidtbootle@gmail.com",
